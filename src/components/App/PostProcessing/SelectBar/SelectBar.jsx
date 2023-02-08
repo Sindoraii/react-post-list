8c34aeccc24f181db6f1991ad94b52408547635e
Mariya Krupenina <marise.sindorai@yandex.ru>
2023-02-08 10:33:37 +0300

    feat: add dependency between selectBar state and its rendering, refactor idents

--- src/components/App/PostProcessing/SelectBar/SelectBar.jsx
+++ src/components/App/PostProcessing/SelectBar/SelectBar.jsx
@@ -6,32 +6,52 @@ import styles from "./SelectBar.module.css";
  * @param totalPosts - number of all count of post
  * @param handler - callback for getting count of current posts to PostProcessing
  */
-function SelectBar({totalPosts,handler}) {
-    const [active,setActive] = useState(false);
-    const values = [50,25,10];
+function SelectBar({totalPosts, handler}) {
+    const [active, setActive] = useState(false);
+    const values = [50, 25, 10];
 
 
     function getCurrentValue(e) {
-        if(e.target.value === undefined) {
+        if (e.target.value === undefined) {
             handler(totalPosts)
         } else {
             handler(e.target.value)
         }
     }
 
-    return(
+    return (
         <>
-            <div className={styles.select}>
-                <header className={styles.header}
-                    onClick={() => setActive(true)}>
-                    <h3>Post count:</h3>
-                    <Arrow isActive={active}/>
-                </header>
-                <ul onClick={(e)=>getCurrentValue(e)}>
-                    <li className={styles.item} value={totalPosts}>{totalPosts}</li>
-                    {values.map((value) => <li key={value} className={styles.item} value={value}>{value}</li>)}
-                </ul>
-            </div>
+            {active ?
+                (
+                    <div className={styles.select}>
+                        <header className={styles.header}
+                                onClick={() => {
+                                    active ? setActive(false) : setActive(true)
+                                }}>
+                            <h3>Post count:</h3>
+                            <Arrow isActive={active}/>
+                        </header>
+
+                        <ul onClick={(e) => {
+                            getCurrentValue(e);
+                            setActive(false)
+                        }}>
+                            <li className={styles.item} value={totalPosts}>{totalPosts}</li>
+                            {values.map((value) => <li key={value} className={styles.item} value={value}>{value}</li>)}
+                        </ul>
+                    </div>
+                ) : (
+                    <div className={styles.select}>
+                        <header className={styles.header}
+                                onClick={() => {
+                                    active ? setActive(false) : setActive(true)
+                                }}>
+                            <h3>Post count:</h3>
+                            <Arrow isActive={active}/>
+                        </header>
+                    </div>
+                )
+            }
         </>
     )
 }
